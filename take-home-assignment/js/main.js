5f31b6185bf96e7a80be8d1430b13e5e0fac0cd1
Anxhelo Lushka <anxhelo1995@gmail.com>
2021-04-11 21:22:43 +0200

    Added first part of the mobile media queries
    Added some JS logic to handle the mobile hamburger menu

---
+++ take-home-assignment/js/main.js
@@ -0,0 +1,15 @@
+// Mobile menu
+
+const menuTrigger = document.querySelector(".trigger");
+const menu = document.querySelector("nav");
+const mobileQuery = getComputedStyle(document.body).getPropertyValue(
+  "--phoneWidth"
+);
+
+const isMobile = () => window.matchMedia(mobileQuery).matches;
+const isMobileMenu = () => {
+  menuTrigger && menuTrigger.classList.toggle("hidden", !isMobile());
+  menu && menu.classList.toggle("hidden", isMobile());
+};
+
+isMobileMenu();
